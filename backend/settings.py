def8c4baa04cf690de028dfb11290195e6433c5e
fady668 <fadygeorge668@gmail.com>
2024-06-10 03:18:33 +0300

    some editions

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y&bhsg8)%8wd0=h9+t^+vkef8ixrf6j@sas-xu7zap4))ooeb_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
